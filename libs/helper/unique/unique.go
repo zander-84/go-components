8ef1fa6353e5168815ac4ac9b72aa63259b02feb
zander <676993761@qq.com>
2020-03-11 21:12:27 +0800

    modity

--- libs/helper/unique/unique.go
+++ libs/helper/unique/unique.go
@@ -10,3 +10,7 @@ func (h *Unique) ID() string {
 	guid := xid.New()
 	return guid.String()
 }
+
+//x1:=65+rand.Intn(10)
+//x2:=65+rand.Intn(26)
+//fmt.Println(fmt.Sprintf("%c%c_%03d%s",x1,x2,9999,time.Now().Format("150405")))
