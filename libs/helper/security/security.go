c10182f2ece8c3b3f29d6e401e06d7d44889377c
zander <676993761@qq.com>
2020-06-24 22:29:51 +0800

    add Sha256Hmac

--- libs/helper/security/security.go
+++ libs/helper/security/security.go
@@ -1,14 +1,15 @@
 package CHelperSecurity
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 )
 
-type Security struct {}
+type Security struct{}
 
-func NewSecurity() interface{} {return new(Security)}
+func NewSecurity() interface{} { return new(Security) }
 
 func (*Security) Sum256(str string) string {
 	data := sha256.Sum256([]byte(str))
@@ -19,4 +20,10 @@ func (*Security) Md5(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func (*Security) Sha256Hmac(str string, key string) string {
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(str))
+	return hex.EncodeToString(mac.Sum(nil))
 }
