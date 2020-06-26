382dc75be046d44e2fa0f11f4ca9f29a60595b7a
zander <676993761@qq.com>
2020-06-26 12:55:41 +0800

    req

--- libs/helper/request/req.go
+++ libs/helper/request/req.go
@@ -93,6 +93,37 @@ func (this *HttpCli) Do(method string, url string, reqFunc func(r *http.Request)
 	}
 }
 
+func (this *HttpCli) ClientDo(client *http.Client, method string, url string, reqFunc func(r *http.Request), bodyReader io.Reader, debug bool) (http.Header, []byte, error) {
+
+	req, err := http.NewRequest(strings.ToUpper(method), url, bodyReader)
+	if err != nil {
+		return nil, nil, err
+	}
+	if reqFunc != nil {
+		reqFunc(req)
+	}
+	//req.Close = true
+
+	//It looks like the that server (Apache 1.3, wow!) is serving up a truncated gzip response. If you explicitly request the identity encoding (preventing the Go transport from adding gzip itself), you won't get the ErrUnexpectedEOF:
+	//req.Header.Add("Accept-Encoding", "identity")
+
+	if debug {
+		dump, _ := httputil.DumpRequest(req, debug)
+		fmt.Println(string(dump))
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, nil, err
+	} else {
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		return resp.Header, body, err
+	}
+}
+
 var ErrNoRedirect = errors.New("Don't redirect!")
 
 func (this *HttpCli) DoOrigin(method string, url string, reqFunc func(r *http.Request), bodyReader io.Reader) (*http.Response, error) {
