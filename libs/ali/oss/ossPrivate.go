ecc60b474cdde0c87738963076aeab1f393769bf
zander <676993761@qq.com>
2020-10-07 14:33:00 +0800

    oss

--- libs/ali/oss/ossPrivate.go
+++ libs/ali/oss/ossPrivate.go
@@ -1,11 +1,20 @@
 package CAliOss
 
+import "strings"
+
 var _ossPrivate = new(OssPrivate)
 
 type OssPrivate struct {
 	Oss
 }
 
+func (this *OssPrivate) construct(conf Conf) *OssPrivate {
+	this.conf = conf
+	if this.conf.Dir != "" {
+		this.conf.Dir = strings.Trim(this.conf.Dir, "/") + "/"
+	}
+	return this
+}
 func NewOssPrivate(conf Conf) *OssPrivate {
 	_ossPrivate.construct(conf)
 	return _ossPrivate
